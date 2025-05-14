6a8d6c510ba0b3175814b5ff96ce95a60272acd3
itc-n25013 <n25013@std.it-college.ac.jp>
2025-05-14 06:07:59 +0000

    P15 第2章 チャレンジ問題終了

---
+++ Chapter02/challenge2-01.py
@@ -0,0 +1,9 @@
+print("Powfuおすすめです。")
+print("Eleanor & Bellami, BEAUTIFUL AS EVER")
+
+s = "How To Live"
+d = "clash of the titans"
+print(f"{s}, {d}")
+
+f = "Khalid"
+print (f"他にも{f}のTalkおすすめです")
